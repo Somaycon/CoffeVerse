70e43b829da21271454c6e0d44e0ff21a555cfd7
joaonascimentooo <joaonascimento197@gmail.com>
2024-11-27 18:10:30 -0300

    bug fix/ app.py

--- coffeverse/app.py
+++ coffeverse/app.py
@@ -2,7 +2,11 @@ from flask import Flask
 from coffeverse.extensions import config
 
 def create_app():
-  app = Flask(__name__)
-  config.init_app(app)
-  config.load_extentions(app)
-  return app
+    app = Flask(__name__)
+    config.init_app(app)
+    config.load_extentions(app)
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
